f3e1b0ef270ff08d27b724455963c9aff10fe3bf
HH <heather.hayes918@gmail.com>
2023-03-25 16:11:44 -0500

    task4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ displays Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ displays HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cis(text):
+    """ displays C followed by the value of the text variable """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+def pyis(text):
+    """ displays Python followed by the value of the text variable """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def num(n):
+    """ displays n is a number only if n is an integer """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
