91e6bfea866ead3ec8c6c7d095531c8fa2420e2d
HH <heather.hayes918@gmail.com>
2023-03-27 18:39:03 -0500

    pleassssse

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-""" must use storage for fetching data
-from the storage engine (FileStorage or DBStorage) =>
-from models import storage and storage.all(...) """
-from flask import Flask, render_template
+"""
+You must use storage for fetching data from
+the storage engine (FileStorage or DBStorage) =>
+from models import storage and storage.all(...)
+"""
 from models import storage
-from models.state import State
-
+from flask import render_template, Flask
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    """ /states_list: display a HTML page: (inside the tag BODY) """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    """
+    /states_list: display a HTML page: (inside the tag BODY)
+    """
+    states = storage.all('State')
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def close_session(exc):
-    """ After each request you must remove the current SQLAlchemy Session:
+def teardown(exc):
+    """
+    After each request you must remove the current SQLAlchemy Session:
     Declare a method to handle @app.teardown_appcontext
-    Call in this method storage.close() """
+    Call in this method storage.close()
+    """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
