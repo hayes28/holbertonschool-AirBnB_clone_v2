fdc779a5ccf87a1a5afa84b4346e7c1d81cdd5a2
HH <heather.hayes918@gmail.com>
2023-03-27 15:48:51 -0500

    wtf

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from datetime import datetime
+from models import city, place, review, state, amenity, user, base_model
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+    CDIC = {
+        'City': city.City,
+        'Place': place.Place,
+        'Review': review.Review,
+        'State': state.State,
+        'Amenity': amenity.Amenity,
+        'User': user.User
+    }
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """Returns a dictionary of models currently in storage
+        if cls specified, only returns that class"""
         if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
-            spec_dict = {}
-            for k, v in self.__objects.items():
-                if cls == type(v):
-                    spec_dict[k] = v
-            return spec_dict
+            if cls in self.CDIC.keys():
+                cls = self.CDIC.get(cls)
+            spec_rich = {}
+            for ky, vl in self.__objects.items():
+                if cls == type(vl):
+                    spec_rich[ky] = vl
+            return spec_rich
         return self.__objects
 
     def new(self, obj):
@@ -26,13 +35,13 @@ class FileStorage:
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
+        """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
-        json.dump(temp, f)
-
+            json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,10 +54,10 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-            }
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -59,9 +68,12 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """ delete obj from __objects """
+        """if obj deletes obj from __objects"""
         try:
             key = obj.__class__.__name__ + "." + obj.id
             del self.__objects[key]
         except (AttributeError, KeyError):
             pass
+
+    def close(self):
+        self.reload()
