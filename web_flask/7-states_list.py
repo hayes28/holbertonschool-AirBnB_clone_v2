f5e968a55526a0c0539ec17016c878ffc5515548
HH <heather.hayes918@gmail.com>
2023-03-29 11:49:54 -0500

    doc string changes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 """
 Script that starts a Flask web application listening on 0.0.0.0:5000
-Use storage for fetching data from the storage engine File or DB storage
-from models import storage and storage.all(...)
-After each request, remove current SQLAlchemy Session:
-    Declare method to handle @app.teardown_appcontext
-    Call in this method: storage.close()
-Route /states_list: display a HTML page
-    H1 tag: "States"
-    UL tag: list of all State objects present in DBStorage sorted by name
-        LI tag: Description of one State: <state.id>: <<B><state.name></B>
-Must use option strict_slashes=False
 """
 from flask import Flask, render_template
 from models import storage
