1dd6642edc441e11f674c8636424ff49d3bd0f90
HH <heather.hayes918@gmail.com>
2023-03-30 17:24:28 -0500

    'States' not found fixed?

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Start a Flask web application """
+""" Starts new Flask web application """
 
 from flask import Flask, render_template
 from models import storage
@@ -7,27 +7,31 @@ from models import storage
 app = Flask(__name__)
 
 
+def StartFlask():
+    """ Starts Flask web application """
+    app.run(host='0.0.0.0', port=5000)
+
+
 @app.route('/states', strict_slashes=False)
 def states_list():
-    """ Display List of States """
+    """ Displays a List of States """
     states = storage.all('State')
-    return render_template('9-states.html', state=states)
+    return render_template('9-states.html', states=states.values(), id=None)
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
-    """ Display List of States """
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+    """ Displays a List of States """
+    states = storage.all('State')
+    state = states.get('State.' + id)
+    return render_template('9-states.html', states=state, my_id=id)
 
 
 @app.teardown_appcontext
-def teardown_db(exc):
-    """ Remove the current SQLAlchemy Session """
+def teardown_db(exception):
+    """ Remove current SQLAlchemy Session """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    StartFlask()
