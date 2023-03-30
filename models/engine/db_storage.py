95f0c95f3b17d07743a63941701174b9e211c3ac
HH <heather.hayes918@gmail.com>
2023-03-30 15:35:27 -0500

    chmod

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage sql storage for hbnb clone"""
+""" This module defines a class to manage sql storage for hbnb clone """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
