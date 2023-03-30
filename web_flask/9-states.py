0cc40218da298573da07c1d09b3586578670cbff
HH <heather.hayes918@gmail.com>
2023-03-30 17:06:25 -0500

    added states manually

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
-from flask import Flask, render_template
-from models import storage
+""" Start a Flask web application """
 
+from flask import Flask
+from flask import render_template
+from models import storage
 
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
+def StartFlask():
+    """ Start a Flask web application """
+    app.run(host='0.0.0.0', port=5000)
+
+
+@app.route('/states/', strict_slashes=False)
 def states_list():
-    """ 10. States and State """
+    """ Display List of States """
     states = storage.all('State')
-    return render_template("9-states.html", states=states.values(),
-                           id=None)
+    return render_template('9-states.html', states=states.values(), id=None)
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
-    """ 10. States and State """
+    """ Display List of States """
     states = storage.all('State')
     state = states.get('State.' + id)
     return render_template('9-states.html', states=state, my_id=id)
 
 
 @app.teardown_appcontext
-def teardown_db(exc):
-    """ 10. States and State """
+def teardown_db(exception):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    StartFlask()
