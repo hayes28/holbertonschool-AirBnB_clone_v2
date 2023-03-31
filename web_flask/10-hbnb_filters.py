c4425e8f506fb70305542426c16f662b6991fd70
HH <heather.hayes918@gmail.com>
2023-03-30 19:47:50 -0500

    config folder placement

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -4,35 +4,32 @@ Start a Flask web application
 """
 from flask import Flask, render_template
 from models import storage
+from models.amenity import Amenity
+from models.state import State
+
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-def StartFlask():
+@app.route("/hbnb_filters")
+def hbnb_filters():
     """
-    Start Flask web app
+    Displays an HTML formatted of cities with a given State id
     """
-    app.run(host='0.0.0.0', port=5000)
-
-
-@app.route('/', strict_slashes=False)
-def show_pages():
-    """
-    Show all pages
-    """
-    states = storage.all('State')
-    amenities = storage.all('Amenity')
-    return render_template('10-hbnb_filters.html', states=states.values(),
-                           amenities=amenities.values())
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html", state_list=states,
+                           amenity_list=amenities)
 
 
 @app.teardown_appcontext
-def teardown_db(exc):
+def teardown(stuff):
     """
-    Close storage
+    Remove current SQLAlchemy session
     """
     storage.close()
 
 
 if __name__ == "__main__":
-    StartFlask()
+    app.run(host="0.0.0.0", port="5000")
